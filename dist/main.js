583328df51d921ff34462dac29d9aff7ea1224ab
Hariprasath12 <Hariprasath.1295@gmail.com>
2016-11-06 18:53:12 +0530

    first

--- dist/main.js
+++ dist/main.js
@@ -1,29 +1,32 @@
-var Review = (function() {
-    function Review() {}
-    Review.prototype.addreviewForMovie = function(nameForMovie, starForMovie, cmdForMovie) {
-        var a = {
-            Movie: "i",
-            star: "2",
-            cmd: "a"
-        };
+// import mo = require("colors/lib/colors.js");
+// import * as fs from "colors";
+var Review = (function () {
+    function Review() {
+    }
+    Review.prototype.addreviewForMovie = function (nameForMovie, starForMovie, cmdForMovie) {
+        var a = { Movie: "i", star: "2", cmd: "a" };
         a.Movie = nameForMovie;
         a.star = starForMovie;
         a.cmd = cmdForMovie;
         Review.reviewdb.push(a);
+        alert("sucessfully added");
         console.log("sucessfully added");
+        console.log("_____________________________________________________________");
     };
-    Review.prototype.reviewFormovie = function(Moviename) {
-        var movie = Review.reviewdb.filter(function(i) {
-            return i.Movie == Moviename;
-        });
-        console.log(movie);
+    Review.prototype.reviewFormovie = function (Moviename) {
+        console.log("Movie Review");
+        var movie = Review.reviewdb.filter(function (i) { return i.Movie == Moviename; });
+        return movie;
+        // movie.forEach((i:any) =>console.log(i.Movie+'  '+i.star+'  '+i.cmd));
+        // console.log("_____________________________________________________________");
     };
     Review.reviewdb = [];
     return Review;
 }());
-var payment = (function() {
-    function payment() {}
-    payment.prototype.addshowForMovie = function(nameForMovie, placeForMovie, priceForMovie, show, screenname, seats) {
+var payment = (function () {
+    function payment() {
+    }
+    payment.prototype.addshowForMovie = function (nameForMovie, placeForMovie, priceForMovie, show, screenname, seats) {
         var a = {};
         a.Movie = nameForMovie;
         a.place = placeForMovie;
@@ -32,54 +35,68 @@ var payment = (function() {
         a.Nameofscreen = screenname;
         a.nOfSeats = seats;
         payment.paymentdb.push(a);
+        alert("Show added");
     };
-    payment.prototype.makepaymentFormovie = function(Moviename, place, screen, seats, show) {
-        var movie = payment.paymentdb.filter(function(i) {
-            return (i.Movie == Moviename && i.place == place);
-        });
+    payment.prototype.makepaymentFormovie = function (Moviename, place, screen, seats, show) {
+        var movie = payment.paymentdb.filter(function (i) { return (i.Movie == Moviename && i.place == place); });
         // movie.forEach((i:any) =>console.log(i));
         if (movie.length !== 0) {
-            var se = movie.filter(function(i) {
-                return i.show == show;
-            });
+            var se = movie.filter(function (i) { return i.show == show; });
             // se.forEach((i:any) =>console.log(i));
             if (se.length !== 0) {
-                var me = se.filter(function(i) {
-                    return i.nOfSeats >= seats;
-                });
+                var me = se.filter(function (i) { return i.nOfSeats >= seats; });
                 if (me.length !== 0) {
                     for (var i in payment.paymentdb) {
                         if (payment.paymentdb[i].Movie == Moviename && payment.paymentdb[i].place == place && payment.paymentdb[i].show == show) {
                             payment.paymentdb[i].nOfSeats = payment.paymentdb[i].nOfSeats - seats;
                             console.log("Movie Booked");
+alert("Movie Booked");
+
+                            console.log("_____________________________________________________________");
                             break;
                         }
                     }
-                } else {
-                    console.log("seats are full");
-                    console.log("available in ");
                 }
-            } else {
-                console.log("show is not available");
-                console.log("available in ");
+                else {
+                    alert("seats are full");
+                    console.log("seats are full");
+                    
+
+                    console.log("_____________________________________________________________");
+                }
             }
-        } else {
+            else {
+                alert("show is not available");
+                console.log("show is not available");
+               
+                console.log("_____________________________________________________________");
+            }
+        }
+        else {
+            alert("Movie is not playing in that screen");
             console.log("Movie is not playing in that screen");
+            console.log("_____________________________________________________________");
         }
     };
-    payment.prototype.alldetails = function() {
+    payment.prototype.alldetails = function () {
         console.log("Movie     place      show       Name of screen   available seats");
-        for (var _i = 0, _a = payment.paymentdb; _i < _a.length; _i++) {
-            var value = _a[_i];
-            console.log(value.Movie + '       ' + value.place + '        ' + value.show + '        ' + value.Nameofscreen + '     ' + value.nOfSeats);
-        }
+        //       for (let value of payment.paymentdb) {
+        //  console.log(value.Movie+'       '+value.place+'        '+value.show+'        '+value.Nameofscreen+'     '+value.nOfSeats);
+        // }
+        // let a=payment.paymentdb
+        return payment.paymentdb;
+        //    for (let value of payment.paymentdb) {
+        //  console.log(value.Movie+'       '+value.place+'        '+value.show+'        '+value.Nameofscreen+'     '+value.nOfSeats);
+        // }
+        // console.log("__________________________________________________________________________");
     };
     payment.paymentdb = [];
     return payment;
 }());
-var search = (function() {
-    function search() {}
-    search.prototype.addSearch = function(nameForMovie, placeForMovie, priceForMovie, show, screenname, seats) {
+var search = (function () {
+    function search() {
+    }
+    search.prototype.addSearch = function (nameForMovie, placeForMovie, priceForMovie, show, screenname, seats) {
         var a = {};
         a.Movie = nameForMovie;
         a.place = placeForMovie;
@@ -89,58 +106,65 @@ var search = (function() {
         a.nOfSeats = seats;
         search.searchdb.push(a);
     };
-    search.prototype.listOfMovies = function(place) {
+    search.prototype.listOfMovies = function (place) {
         // var mov=search.searchdb
-        var mov = search.searchdb.filter(function(i) {
-            return (i.place == place);
-        });
-        for (var _i = 0, mov_1 = mov; _i < mov_1.length; _i++) {
-            var value = mov_1[_i];
-            console.log(value.Movie);
-        }
+        var mov = search.searchdb.filter(function (i) { return (i.place == place); });
+        return mov;
+        // for (let value of mov) {
+        //   console.log(value.Movie);
+        // }
     };
-    search.prototype.listofSceen = function(place, movie) {
-        var mov = search.searchdb.filter(function(i) {
-            return (i.place == place && i.Movie == movie);
-        });
-        for (var _i = 0, mov_2 = mov; _i < mov_2.length; _i++) {
-            var value = mov_2[_i];
-            console.log(value.Nameofscreen);
-        }
+    search.prototype.listofSceen = function (place, movie) {
+        var mov = search.searchdb.filter(function (i) { return (i.place == place && i.Movie == movie); });
+        return mov;
+        //    for (let value of mov) {
+        //      console.log("Screen Name");
+        //   console.log(value.Nameofscreen);
+        //      console.log("_____________________________________________________________");
+        // }
     };
     search.searchdb = [];
     return search;
 }());
-var system = (function() {
+var system = (function () {
     function system() {
         this.r = new Review();
         this.s = new search();
         this.p = new payment();
     }
-    system.prototype.adddetails = function(movie, place, screen, price, show, seats) {
+    system.prototype.adddetails = function (movie, place, screen, price, show, seats) {
         this.s.addSearch(movie, place, price, show, screen, seats);
         this.p.addshowForMovie(movie, place, price, show, screen, seats);
     };
-    system.prototype.Alldetails = function() {
-        this.p.alldetails();
+    system.prototype.getreview = function (movie) {
+        return this.r.reviewFormovie(movie);
     };
-    system.prototype.searchByPlace = function(place) {
-        this.s.listOfMovies(place);
+    system.prototype.setreview = function (movie, star, cmd) {
+        this.r.addreviewForMovie(movie, star, cmd);
     };
-    system.prototype.searchByMovie = function(place, movie) {
-        this.s.listofSceen(place, movie);
+    system.prototype.Alldetails = function () {
+        return this.p.alldetails();
     };
-    system.prototype.makePayment = function(Moviename, place, screen, seats, show) {
+    system.prototype.searchByPlace = function (place) {
+        return this.s.listOfMovies(place);
+    };
+    system.prototype.searchByMovie = function (place, movie) {
+        return this.s.listofSceen(place, movie);
+    };
+    system.prototype.makePayment = function (Moviename, place, screen, seats, show) {
         this.p.makepaymentFormovie(Moviename, place, screen, seats, show);
     };
     return system;
 }());
-var sa = new system();
-sa.adddetails("remo", "vellore", "galaxy", 120, "evening", 120);
-sa.adddetails("i", "vellore", "galaxy", 120, "evening", 120);
-sa.adddetails("kodi", "vellore", "galaxy", 120, "evening", 120);
-sa.adddetails("okok", "vellore", "galaxy", 120, "evening", 120);
-sa.searchByMovie("vellore", "i");
+
+// var sa = new system();
+// sa.adddetails("remo", "vellore", "galaxy", 120, "evening", 120);
+// sa.adddetails("i", "vellore", "galaxy", 120, "evening", 120);
+// sa.adddetails("kodi", "vellore", "l", 120, "evening", 120);
+// sa.adddetails("okok", "vellore", "v", 120, "evening", 120);
+// sa.setreview("i", 3, "nil");
+
+// console.log(sa.getreview("i"));
 // sa.makePayment("okok","vellore","galaxy",3,"evening");
 // sa.makePayment("okok","vellore","galaxy",119,"evening");
-sa.Alldetails();
+// console.log(sa.Alldetails());
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 
-
+// import mo = require("colors/lib/colors.js");
+// import * as fs from "colors";
 class Review {
          private static reviewdb:any=[];
       
@@ -10,12 +11,16 @@ class Review {
         a.cmd=cmdForMovie;
         Review.reviewdb.push(a);
         console.log("sucessfully added");
+     console.log("_____________________________________________________________");
+
  }
  reviewFormovie(Moviename:string) {
-   
+    console.log("Movie Review");
      let movie= Review.reviewdb.filter((i:any) => i.Movie==Moviename);
+     return movie;
+            // movie.forEach((i:any) =>console.log(i.Movie+'  '+i.star+'  '+i.cmd));
 
-     console.log(movie);
+     // console.log("_____________________________________________________________");
 
  }
 }
@@ -50,6 +55,8 @@ class payment {
      if (payment.paymentdb[i].Movie == Moviename && payment.paymentdb[i].place==place && payment.paymentdb[i].show==show ) {
         payment.paymentdb[i].nOfSeats =payment.paymentdb[i].nOfSeats-seats;
         console.log("Movie Booked");
+     console.log("_____________________________________________________________");
+
         break; 
      }
    }
@@ -60,28 +67,41 @@ class payment {
                      else{
                    console.log("seats are full");
                   console.log("available in ");
+     console.log("_____________________________________________________________");
+
                   // se.forEach(i=>console.log(se[i].show));
                      }
                 }
                 else{
                   console.log("show is not available");
                   console.log("available in ");
+     console.log("_____________________________________________________________");
+
                   // se.forEach((i,x)=>console.log(x[i].show));
                 }
                
            }
            else{
                console.log("Movie is not playing in that screen");
+     console.log("_____________________________________________________________");
+
            }
 
 
        } 
      
-      alldetails(){
+      alldetails():Object{
    console.log("Movie     place      show       Name of screen   available seats");
-   for (let value of payment.paymentdb) {
- console.log(value.Movie+'       '+value.place+'        '+value.show+'        '+value.Nameofscreen+'     '+value.nOfSeats);
-}
+//       for (let value of payment.paymentdb) {
+//  console.log(value.Movie+'       '+value.place+'        '+value.show+'        '+value.Nameofscreen+'     '+value.nOfSeats);
+// }
+// let a=payment.paymentdb
+   return payment.paymentdb;
+
+//    for (let value of payment.paymentdb) {
+//  console.log(value.Movie+'       '+value.place+'        '+value.show+'        '+value.Nameofscreen+'     '+value.nOfSeats);
+// }
+// console.log("__________________________________________________________________________");
  }
      
 
@@ -103,17 +123,21 @@ class search {
    listOfMovies(place:string){
      // var mov=search.searchdb
      var mov= search.searchdb.filter((i:any)  => ( i.place==place) );
-
-for (let value of mov) {
-  console.log(value.Movie);
-}
+ return mov;
+// for (let value of mov) {
+//   console.log(value.Movie);
+// }
 
    }
  listofSceen(place:string,movie:string){
      var mov= search.searchdb.filter((i:any)  => ( i.place==place && i.Movie==movie) );
-   for (let value of mov) {
-  console.log(value.Nameofscreen);
-}
+     return mov;
+//    for (let value of mov) {
+//      console.log("Screen Name");
+//   console.log(value.Nameofscreen);
+//      console.log("_____________________________________________________________");
+
+// }
  }
 
  
@@ -138,15 +162,21 @@ class system{
         this.p.addshowForMovie(movie,place,price,show,screen,seats);
 
     }
+    getreview(movie:string){
+      return this.r.reviewFormovie(movie);
+    }
+    setreview(movie:string,star:number,cmd:string){
+      this.r.addreviewForMovie(movie,star,cmd);
+    }
     Alldetails(){
-      this.p.alldetails();
+      return this.p.alldetails();
     }
     searchByPlace(place:string){
-     this.s.listOfMovies(place);
+      return this.s.listOfMovies(place);
 
     }
     searchByMovie(place:string,movie:string){
-      this.s.listofSceen(place,movie);
+      return this.s.listofSceen(place,movie);
     }
     makePayment(Moviename:string,place:string,screen:string,seats:number,show:string){
      this.p.makepaymentFormovie(Moviename,place,screen,seats,show);
@@ -159,9 +189,10 @@ sa.adddetails("remo","vellore","galaxy",120,"evening",120);
 sa.adddetails("i","vellore","galaxy",120,"evening",120);
 sa.adddetails("kodi","vellore","galaxy",120,"evening",120);
 sa.adddetails("okok","vellore","galaxy",120,"evening",120);
-
+sa.setreview("i",3,"nil");
+sa.getreview("i");
 sa.searchByMovie("vellore","i");
 
 // sa.makePayment("okok","vellore","galaxy",3,"evening");
 // sa.makePayment("okok","vellore","galaxy",119,"evening");
-sa.Alldetails();
+console.log(sa.Alldetails());
